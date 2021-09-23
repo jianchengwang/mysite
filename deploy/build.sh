08021747800379e2114806eb1fe309278db7f8a1
jianchengwang <jiancheng_wang@yahoo.com>
2021-09-24 00:21:05 +0800

    test build 1

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Get real path
+BASEDIR=$(cd `dirname $0` && pwd)
+cd ${BASEDIR}
+
 # Log Location on Server.
 LOG_LOCATION=${BASEDIR}
 exec > >(tee -i $LOG_LOCATION/build.`date +%Y%m%d%H%M%S`.log)
@@ -14,6 +20,8 @@ yarn generate
 
 # 编译go
 cd ../
+go env -w GOPROXY=https://goproxy.cn,direct
+go mod vendor
 set GOARCH=amd64
 set GOOS=linux
 go build -o mysite main.go
@@ -25,6 +33,8 @@ docker login
 docker push jianchengwang/mysite
 
 # 部署
+cd ${BASEDIR}
+kill -9 `netstat -nlp | grep :8081 | awk '{print $7}' | awk -F"/" '{ print $1 }'`
 docker pull jianchengwang/mysite:latest
 docker-compose up -d
 
