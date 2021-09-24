443067699f03a2eace84607ee98bb1079c859fb5
jianchengwang <jiancheng_wang@yahoo.com>
2021-09-24 18:06:25 +0800

    test build 5

--- nuxtsite/pages/tools/downgit.vue
+++ nuxtsite/pages/tools/downgit.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="main">
-    <h1 class="main-title">DownGit</h1>
+    <h1 class="main-title">
+      DownGit
+    </h1>
     <div class="main-body">
-      <input placeholder="GitHub 文件 或者文件夹 链接" v-model="gitrep" />
+      <input v-model="gitRep" placeholder="GitHub 文件 或者文件夹 链接">
       <div class="button-group">
         <a href="javascript:void(0);" @click="download">Download</a>
       </div>
@@ -11,45 +13,45 @@
 </template>
 <script>
 export default {
-  name: "T-DownGit",
+  name: 'TDownGit',
   components: {},
-  data() {
+  data () {
     return {
-      gitrep: "",
-    };
+      gitRep: ''
+    }
   },
   methods: {
-    download: async function () {
-      if (this.gitrep && this.gitrep.startsWith("https://github.com/")) {
-        this.$toast.show("Loading...");
-        let response = await fetch(`/api/downgit?gitrep=${this.gitrep}`);
-        let downloadLink = await response.text();
-        downloadLink = downloadLink.replace(/\"/g, "");
-        downloadLink = downloadLink.substring(0, downloadLink.length - 2);
-        if (downloadLink && downloadLink.length>0) {
+    async download () {
+      if (this.gitRep && this.gitRep.startsWith('https://github.com/')) {
+        this.$toast.show('Loading...')
+        const response = await fetch(`/tools/downGit?gitRep=${this.gitRep}`)
+        let downloadLink = await response.text()
+        downloadLink = downloadLink.replace(/"/g, '')
+        downloadLink = downloadLink.substring(0, downloadLink.length - 2)
+        if (downloadLink && downloadLink.length > 0) {
           this.$toast.show({
-            type: "success",
-            title: "Success",
-            message: "已生成下载链接，即将下载",
-          });
-          window.open(downloadLink);
+            type: 'success',
+            title: 'Success',
+            message: '已生成下载链接，即将下载'
+          })
+          window.open(downloadLink)
         } else {
           this.$toast.show({
-            type: "danger",
-            title: "Error",
-            message: "下载出现错误，请稍后再试",
-          });
+            type: 'danger',
+            title: 'Error',
+            message: '下载出现错误，请稍后再试'
+          })
         }
       } else {
         this.$toast.show({
-          type: "danger",
-          title: "Error",
-          message: "请输入github文件地址",
-        });
+          type: 'danger',
+          title: 'Error',
+          message: '请输入github文件地址'
+        })
       }
-    },
-  },
-};
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 input {
