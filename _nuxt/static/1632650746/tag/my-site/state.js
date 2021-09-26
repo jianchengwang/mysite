13ea119433e919b8f651c26cbbb4e8828e85e9bd
jianchengwang <jianchengwang@users.noreply.github.com>
2021-09-26 10:06:02 +0000

    deploy: 5eb860cd70455179ee8984d54b8578068133b90e

---
+++ _nuxt/static/1632650746/tag/my-site/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632650746",layout:"default",error:null,state:{siedeBar:a},serverRendered:b,routePath:"\u002Ftag\u002Fmy-site",config:{content:{dbHash:"e0958bb9"}},colorMode:{preference:c,value:c,unknown:b,forced:a}}}(false,true,"system"));
