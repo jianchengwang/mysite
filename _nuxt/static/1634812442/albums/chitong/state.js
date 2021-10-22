8c0e151821293a99fa745a56a82c64ef0f38fc5d
jianchengwang <jianchengwang@users.noreply.github.com>
2021-10-22 10:43:07 +0000

    deploy: 2ba623add0c0783bb0e7f5c820a5f508285245a7

--- _nuxt/static/1634812442/albums/chitong/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634812442",layout:"default",error:null,state:{siedeBar:a},serverRendered:b,routePath:"\u002Falbums\u002Fchitong",config:{content:{dbHash:"67c68bc7"}},colorMode:{preference:c,value:c,unknown:b,forced:a}}}(false,true,"system"));
---
+++ _nuxt/static/1634899367/tech/live2d-web/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634899367",layout:"default",error:null,state:{siedeBar:a},serverRendered:b,routePath:"\u002Ftech\u002Flive2d-web",config:{content:{dbHash:"4a603976"}},colorMode:{preference:c,value:c,unknown:b,forced:a}}}(false,true,"system"));
